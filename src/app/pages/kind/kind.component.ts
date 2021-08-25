0fd1cde68f64a93ac35891e63e86a355008212f0
Rerer-er <19it2.pralich.a@pdu.by>
2021-08-25 16:30:16 +0300

    add kind foto

--- src/app/pages/kind/kind.component.ts
+++ src/app/pages/kind/kind.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IGetAllKinds } from 'src/app/interface/IGetAllKinds';
+import { KindService } from 'src/app/service/KindService';
+import { PaginationService } from 'src/app/service/PaginationService';
 
 @Component({
   selector: 'app-kind',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KindComponent implements OnInit {
 
-  constructor() { }
+  public getKinds: IGetAllKinds = {
+    countPage: 1,
+    currentPage:1,
+    kindsDto: []
+  };
+  Params = {
+    searchTerm: "",
+    pageSize: 15,
+    pageNumber: 1,
+    
+  };
+
+  constructor(private kindService : KindService, private router : Router, private pagination: PaginationService) { }
 
   ngOnInit(): void {
+    this.get()
   }
-
+  get(){
+    this.Params.pageNumber = this.pagination.currentPage;
+    this.kindService.ReturnAllKinds(this.Params).subscribe(res => {
+      this.getKinds = res;
+      this.pagination.countAllPage = this.getKinds.countPage;
+    });
+  }
 }
