73c6a720491686e2d7a47b137055f1bb556e3f2b
Rerer-er <19it2.pralich.a@pdu.by>
2021-08-27 09:43:14 +0300

    removed google maps so the map is only in 2branch

--- src/app/pages/pageshipper/pageshipper.component.ts
+++ src/app/pages/pageshipper/pageshipper.component.ts
@@ -2,9 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IShipper } from 'src/app/interface/IShipper';
 import { ShipperService } from 'src/app/service/ShipperService';
-import { google} from "google-maps"
-
-declare var google : google;
 
 @Component({
   selector: 'app-pageshipper',
@@ -24,21 +21,6 @@ export class PageShipperComponent implements OnInit {
     locationLat:0
   }
   ngOnInit(): void{
-
-    var coordinates = {lat: 47.212325, lng: 38.933663},
-
-        map = new google.maps.Map(<HTMLInputElement>document.getElementById('map'), {
-            center: coordinates,
-            zoom: 8
-        }),
-    
-        marker = new google.maps.Marker({
-            position: coordinates,
-            map: map,
-        });
-
-
-
     let id:number = 0;
     this.route.params.subscribe(
       params => {
